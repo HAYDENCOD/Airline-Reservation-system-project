5d2ceef25de15ac12ef8982b10bb9797de5212d2
Paul Webo <paulwebo38@gmail.com>
2022-05-13 11:13:49 +0300

    AirlineReservation project

---
+++ AirLineReservationSystem/src/User_registration.java
@@ -0,0 +1,20 @@
+public class User_registration {
+    static class User{
+        String first_name, last_name;
+        String email;
+        int telephone;
+        int password;
+
+        public User(String first_name, String last_name, String email, int telephone, int password) {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.email = email;
+            this.telephone = telephone;
+            this.password = password;
+        }
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
