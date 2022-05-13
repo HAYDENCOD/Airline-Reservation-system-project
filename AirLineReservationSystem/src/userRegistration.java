eef502fb6531fe0de28cf0b0456be36c0c621ea4
Paul Webo <paulwebo38@gmail.com>
2022-05-13 14:17:35 +0300

    part3

--- AirLineReservationSystem/src/userRegistration.java
+++ AirLineReservationSystem/src/userRegistration.java
@@ -1,19 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class userRegistration extends JDialog {
     private JPanel contentPane;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField5;
-    private JPasswordField passwordField2;
-    private JTextField textField4;
+    private JTextField firstnameText;
+    private JTextField emailtext;
+    private JTextField ageTxt;
+    private JTextField secnameText;
+    private JPasswordField passText1;
+    private JTextField tellText;
     private JLabel first_Name;
     private JLabel email;
     private JLabel age;
     private JLabel password;
-    private JTextField textField6;
-    private JComboBox comboBox1;
+    private JTextField copassText;
+    private JComboBox paymentChoice;
     private JButton signup;
     private JButton signInButton;
     private JButton buttonOK;
@@ -27,6 +31,63 @@ public class userRegistration extends JDialog {
         frame.setSize(500,500);
         frame.setLocationRelativeTo(null);
         frame.pack();
+        int keys = 0;
+        Map<Integer, user> map = new HashMap<>();
+        user person = new user(firstnameText.getText(),secnameText.getText(),tellText.getText(), emailtext.getText(),passText1.getPassword());
+        signup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               map.put(keys,person);
+
+            }
+        });
+    }
+    static class user{
+        String fName;
+        String lName;
+        String tell;
+        String email;
+        char[] password;
+
+        public user(String fName, String lName, String tell, String email, char[] password) {
+            this.fName = fName;
+            this.lName = lName;
+            this.tell = tell;
+            this.email = email;
+            this.password = password;
+        }
+
+        public void setlName(String lName) {
+            this.lName = lName;
+        }
+
+        public void setTell(String tell) {
+            this.tell = tell;
+        }
+
+        public String getfName() {
+            return fName;
+        }
+
+        public void setfName(String fName) {
+            this.fName = fName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public char[] getPassword() {
+            return password;
+        }
+
+        public void setPassword(char[] password) {
+            this.password = password;
+        }
     }
 
     public static void main(String[] args) {
