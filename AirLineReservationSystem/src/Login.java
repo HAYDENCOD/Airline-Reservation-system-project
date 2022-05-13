de18bc0f2b7e3c652d9ca5dac07c5fd5364a3285
Paul Webo <paulwebo38@gmail.com>
2022-05-13 11:45:03 +0300

    first

--- AirLineReservationSystem/src/Login.java
+++ AirLineReservationSystem/src/Login.java
@@ -1,5 +1,13 @@
 
 
-public class Login {
+public class Login extends User_registration {
+    static class login{
+        public login(String firstName, String lastName, int password) {
 
+        }
+    }
+    public static void main(String[] args) {
+
+
+    }
 }
