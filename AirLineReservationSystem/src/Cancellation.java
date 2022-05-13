221ac2a2d84632d95114c4aa19fa773119d867ba
Paul Webo <paulwebo38@gmail.com>
2022-05-13 13:19:07 +0300

    cancellation dialog

--- AirLineReservationSystem/src/Cancellation.java
+++ AirLineReservationSystem/src/Cancellation.java
@@ -1,2 +1,13 @@
-public class Cancellation {
+import javax.swing.*;
+
+public class Cancellation extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    public Cancellation() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+    }
 }
