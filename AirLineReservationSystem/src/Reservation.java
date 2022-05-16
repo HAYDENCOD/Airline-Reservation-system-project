3df33e90a5ad97d42932985d01403ef9ff4cfcc5
Paul Webo <paulwebo38@gmail.com>
2022-05-16 10:16:23 +0300

    reservation

--- AirLineReservationSystem/src/Reservation.java
+++ AirLineReservationSystem/src/Reservation.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class reservation extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+    private JComboBox Destination;
+    private JComboBox currentLocation;
+    private JButton bookFlightButton;
+    private JButton cancelFlightButton;
 
     public reservation() {
         JFrame  frame = new JFrame();
@@ -14,6 +18,21 @@ public class reservation extends JDialog {
         frame.setSize(500,500);
         frame.setLocationRelativeTo(null);
         frame.pack();
+
+        bookFlightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Your Reservation Code is this:");
+            }
+        });
+        cancelFlightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Cancellation now = new Cancellation();
+                now.getClass();
+                frame.dispose();
+            }
+        });
     }
 
     public static void main(String[] args) {
